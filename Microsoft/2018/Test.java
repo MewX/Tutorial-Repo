224830dae11b5ed09fc858ec8c18905b9e2d500b
MewX <imewx@qq.com>
2018-03-13 23:27:53 +1030

    Init 2018 ms competetion

---
+++ Microsoft/2018/Test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.nextInt());
+        s.close();
+    }
+}
